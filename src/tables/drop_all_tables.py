0b029a23cac9296d028701154ac58caaf5d3b7f3
Thomas Bjeldbak Madsen <75805327+thomasbjeldbak@users.noreply.github.com>
2025-11-03 09:42:27 +0100

    Fix null cs stops with poly intersects instead of centroid

--- src/tables/drop_all_tables.py
+++ src/tables/drop_all_tables.py
@@ -1,5 +1,6 @@
 from psycopg import Connection
 
+
 def drop_all_tables(conn: Connection):
     cur = conn.cursor()
 
@@ -7,7 +8,7 @@ def drop_all_tables(conn: Connection):
     # cur.execute("DROP TABLE IF EXISTS prototype1.trajectory_ls;")
     # cur.execute("DROP TABLE IF EXISTS prototype1.stop_poly;")
     # cur.execute("DROP TABLE IF EXISTS prototype1.trajectory_cs;")
-    # cur.execute("DROP TABLE IF EXISTS prototype1.stop_cs;")
+    # cur.execute("DROP TABLE IF EXISTS prototype2.stop_cs;")
     # cur.execute("DROP MATERIALIZED VIEW IF EXISTS prototype1.points;")
 
     print("Dropped all tables and materialized view if they existed.")
